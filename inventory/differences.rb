e48ac39977c0d526a00394fc5deafdec4a4c3b70
onavarroKueski <onavarro@kueski.com>
2018-03-05 13:55:07 -0600

    Adds argument handling and filename downcasing

--- inventory/differences.rb
+++ inventory/differences.rb
@@ -3,8 +3,15 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/bmsft for more book information.
 #---
-old_inventory = File.open('old-inventory.txt').readlines
-new_inventory = File.open('new-inventory.txt').readlines
+#Command line arguments
+unless ARGV.length == 2
+  puts "Need two arguments, one per inventory file" if ARGV.length != 2
+  exit
+end
+old_inventory = File.open(ARGV[0]).readlines.collect { |line| line.downcase }
+#puts old_inventory
+new_inventory = File.open(ARGV[1]).readlines.collect { |line| line.downcase }
+#puts new_inventory
 files_added = (new_inventory - old_inventory).length
 files_removed = (old_inventory - new_inventory).length
 common_count = (new_inventory  & old_inventory).length
